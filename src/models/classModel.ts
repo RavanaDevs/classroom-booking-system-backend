3dfd06c3e8fb6890df2587cf9bbb652959673a0e
Kavindu Sanjula <123143684+RavanaDevs@users.noreply.github.com>
2024-11-04 09:37:55 +0530

    Available resource

--- src/models/classModel.ts
+++ src/models/classModel.ts
@@ -1,18 +1,24 @@
 import { Schema, model, Document } from 'mongoose'
 
-interface IClass extends Document{
-    classRoomName:string
-    seatsCount:number
-    airCondition:String
+interface IClass extends Document {
+  classRoomName: string
+  seatsCount: number
+  airCondition: String
+  availableResources: {
+    _id: string
+    name: string
+    description: string
+  }
 }
 
-const classRoomSchema= new Schema<IClass>(
-    {
-        classRoomName:{type:String,required:true},
-        seatsCount:{type:Number,required:true},
-        airCondition:{type:String,required:true}  
-    },
-    {timestamps:true}
+const classRoomSchema = new Schema<IClass>(
+  {
+    classRoomName: { type: String, required: true },
+    seatsCount: { type: Number, required: true },
+    airCondition: { type: String, required: true },
+    availableResources: { type: Array },
+  },
+  { timestamps: true },
 )
-const ClassRoom=model<IClass>('class',classRoomSchema)
+const ClassRoom = model<IClass>('class', classRoomSchema)
 export default ClassRoom
--- src/validators/classValidator.ts
+++ src/validators/classValidator.ts
@@ -1,10 +1,16 @@
 import { z } from 'zod'
 
 export const classRoomSchema = z.object({
-    classRoomName: z.string(),
-    seatsCount: z.number(),
-    airCondition: z.string(),
-  
+  classRoomName: z.string(),
+  seatsCount: z.number(),
+  airCondition: z.string(),
+  availableResources: z.array(
+    z.object({
+      _id: z.string(),
+      name: z.string(),
+      description: z.string(),
+    }),
+  ),
 })
 
 export default classRoomSchema
